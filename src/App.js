8c23f901a272c4ecbb3e3fd5e85ab384578718a4
yulya_stepanenko <yulichka210901.gmail.com>
2023-07-28 11:38:05 +0300

    Урок №2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,84 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="wrapper clear">
+      <div style={{ display: 'none' }} className="overlay">
+        <div className="drawer">
+          <h2 className="d-flex justify-between mb-30">
+            Кошик <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Remove" />
+          </h2>
+
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                className="cartItemImg"
+                style={{ backgroundImage: "url(/img/sneakers/2.jpg)" }}
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Чоловічі кросівки Nike Air Max 270</p>
+                <b>12 999 грн</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                className="cartItemImg"
+                style={{ backgroundImage: "url(/img/sneakers/2.jpg)" }}
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Чоловічі кросівки Nike Air Max 270</p>
+                <b>12 999 грн</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                className="cartItemImg"
+                style={{ backgroundImage: "url(/img/sneakers/2.jpg)" }}
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Чоловічі кросівки Nike Air Max 270</p>
+                <b>12 999 грн</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+          </div>
+
+          <div class="cartTotalBlock">
+            <ul className="cartTotalBlock">
+              <li>
+                <span>Всього:</span>
+                <div></div>
+                <b>21 498 грн</b>
+              </li>
+              <li>
+                <span>Податок 5%</span>
+                <div></div>
+                <b>1074 грн</b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Оформити замовлення <img src="/img/arrow.svg" alt="Arrow" />{" "}
+            </button>
+          </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" alt="logo" />
@@ -20,69 +96,114 @@ function App() {
             <img width={18} height={18} src="/img/user.svg" alt="user" />
           </li>
         </ul>
-      </header>   
+      </header>
+
       <div className="p-40">
-        <h1 className="mb-40">Всі кросівки</h1>
-        
-
-
-        <div className="d-flex ">
-          <div className="card">
-            <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-            <h5>Чоловічі кросівки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Ціна:</span>
-                <b>12 999 грн</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-              </button>
-            </div>
-          </div>
-          <div className="card">
-            <img width={133} height={112} src="/img/sneakers/2.jpg" alt="Sneakers" />
-            <h5>Чоловічі кросівки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Ціна:</span>
-                <b>12 999 грн</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-              </button>
-            </div>
-          </div>
-          <div className="card">
-            <img width={133} height={112} src="/img/sneakers/3.jpg" alt="Sneakers" />
-            <h5>Чоловічі кросівки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Ціна:</span>
-                <b>12 999 грн</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-              </button>
-            </div>
-          </div>
-          <div className="card">
-            <img width={133} height={112} src="/img/sneakers/4.jpg" alt="Sneakers" />
-            <h5>Чоловічі кросівки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Ціна:</span>
-                <b>12 999 грн</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-              </button>
-            </div>
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Всі кросівки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="search" />
+            <input placeholder="Пошук..." />
           </div>
         </div>
 
-      </div>
+        <div className="d-flex ">
+          <div className="card">
+            <div className="favorite">
+              <img src="/img/heart-unliked.svg" alt="Unliked" />
+            </div>
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/1.jpg"
+              alt="Sneakers"
+            />
+            <h5>Чоловічі кросівки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Ціна:</span>
+                <b>12 999 грн</b>
+              </div>
+              <img
+                className="button"
+                width={11}
+                height={11}
+                src="/img/btn-plus.svg"
+                alt="Plus"
+              />
+            </div>
+          </div>
 
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/2.jpg"
+              alt="Sneakers"
+            />
+            <h5>Чоловічі кросівки Nike Air Max 270</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Ціна:</span>
+                <b>12 999 грн</b>
+              </div>
+              <img
+                className="button"
+                width={11}
+                height={11}
+                src="/img/btn-plus.svg"
+                alt="Plus"
+              />
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/3.jpg"
+              alt="Sneakers"
+            />
+            <h5>Чоловічі кросівки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Ціна:</span>
+                <b>12 999 грн</b>
+              </div>
+              <img
+                className="button"
+                width={11}
+                height={11}
+                src="/img/btn-plus.svg"
+                alt="Plus"
+              />
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/4.jpg"
+              alt="Sneakers"
+            />
+            <h5>Чоловічі кросівки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Ціна:</span>
+                <b>12 999 грн</b>
+              </div>
+              <img
+                className="button"
+                width={11}
+                height={11}
+                src="/img/btn-plus.svg"
+                alt="Plus"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
